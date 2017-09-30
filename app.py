35ba607ef2be390df3280295642b4ccb6f6c8f9f
Matheus Cunha Reis <matheuscunhareis30@gmail.com>
2017-09-30 20:20:16 -0300

    Code Improved

--- app.py
+++ app.py
@@ -1,56 +1,46 @@
 import sys
 import feedparser
-
+import colorama
 
 from colorama import init
-from colorama import Back, Style
+from colorama import Back, Style, Fore
 init(autoreset=True)
 
-# print(Fore.RED + 'some red text')
-# print('automatically back to default color again')
+#Pages Used for Tests
+#http://feeds.foxnews.com/foxnews/latest
+#http://rss.cnn.com/rss/edition.rss
 
-url = sys.argv[1]
-
-
-def latest():
+def latest(quant, opt):
     d = feedparser.parse(url)
-    print (Back.RED + "Title:") + (Style.RESET_ALL + " " + d.entries[0].title)
-    print (Back.YELLOW + "Link:") + (Style.RESET_ALL + " " + d.entries[0].link)
+    for i in range(0, quant):
+        print("\n" + Fore.BLUE + str(i+1))
+        title = d.entries[i].title
+        date = d.entries[i].published if 'published' in d.entries[i] else ""
+        link = d.entries[i].link
+        desc = d.entries[i].description if 'description' in d.entries[i] else ""
+        show(title, date, link, desc, opt)
 
-
-def latest_five():
-    d = feedparser.parse(url)
-    i = 0
-    for i in range(0, 5):
-        print (Back.CYAN + "\t" + str(i + 1) + "\t")
-        print (Back.RED + "Title:") + \
-            (Style.RESET_ALL + " " + d.entries[i].title)
-        print (Back.YELLOW + "Link:") + \
-            (Style.RESET_ALL + " " + d.entries[i].link)
-        # print "Content: " + d.entries[i]['content']
-
-
-def show(title, link, desc):
-    print str(title)
-    # print str(date)
-    print str(link)
-    print str(desc)
-
+#If don't have date or description, it won't generate a error
+def show(title, date, link, desc, flag):
+    print (Fore.BLUE + "Title: " + Style.RESET_ALL + Fore.GREEN + str(title.encode('utf-8')))
+    print (Fore.BLUE + "Date: " + Style.RESET_ALL + Fore.GREEN + str(date.encode('utf-8')))
+    print (Fore.BLUE + "Link: " + Style.RESET_ALL + Fore.GREEN + str(link.encode('utf-8')))
+    if flag == 1:
+        print (Fore.BLUE + "Description: " + Style.RESET_ALL + Fore.GREEN + str(desc.encode('utf-8')))
 
 def menu():
-    print """What do you wish to do now? \n1. Read the latest issue.
-          \n2. Get the titles of the latest 5 issues."""
-    opt = int(raw_input('opt: '))
+    print("What do you wish to do now?")
+    print("1. Read the latest issue.")
+    print("2. Get the title of the latest 5 issues.")
+    opt = int(raw_input('Option: '))
     if opt == 1:
-        latest()
+        latest(1 , opt)
     elif opt == 2:
-        latest_five()
+        latest(5 , opt)
     else:
-        print "Not a valid choice"
+        print("Not a valid choice")
         exit(0)
 
-
 if __name__ == "__main__":
-    # parse(sys.argv[1])
     url = sys.argv[1]
     menu()
